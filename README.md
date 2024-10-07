c0c6108d957de174548f353c431ef79199675da8
Calbabreaker <calbabreaker@gmail.com>
2024-10-08 01:33:34 +1030

    fix: no need to use redis

--- README.md
+++ README.md
@@ -8,12 +8,6 @@ Make temporary links for storing stuff.
 
 1. Install [shuttle](https://docs.shuttle.rs/introduction/installation)
 
-1. Add a `Secrets.toml` file to add the redis url (you can use [upstash](https://upstash.com):
-
-```
-REDIS_URL="rediss://default:*@localhost:6379"
-```
-
 1. Run dev server:
 
 ```sh
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,41 @@
-use std::{collections::HashSet, sync::LazyLock};
+use std::{
+    collections::HashMap,
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 use axum::{
     extract::{DefaultBodyLimit, Path, State},
-    http::{header, HeaderName, StatusCode},
+    http::{header, StatusCode},
     response::{Html, IntoResponse, Response},
     routing::{get, post},
     Json, Router,
 };
-use redis::Commands;
 use serde::{Deserialize, Serialize};
-use tower_governor::governor::GovernorConfigBuilder;
+use shuttle_runtime::tokio;
+use tokio::sync::RwLock;
 
 const LINK_LENGTH: usize = 5;
+const MAX_LINKS: usize = 10000;
+const EXPIRE_DURATION: Duration = Duration::from_secs(5 * 60);
+const CLEANUP_INTERVAL: Duration = Duration::from_secs(30);
 
-pub struct AxumService(pub axum::Router);
-
-/// This is literaly the same as in shuttle-axum but it doesn call into_make_service_with_connect_info so we do it here
-#[shuttle_runtime::async_trait]
-impl shuttle_runtime::Service for AxumService {
-    async fn bind(mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
-        axum::serve(
-            shuttle_runtime::tokio::net::TcpListener::bind(addr).await?,
-            self.0
-                .into_make_service_with_connect_info::<std::net::SocketAddr>(),
-        )
-        .await?;
-        Ok(())
-    }
+struct Link {
+    data: String,
+    expire_time: Instant,
 }
 
+type LinkStore = Arc<RwLock<HashMap<String, Link>>>;
+
 #[shuttle_runtime::main]
-async fn axum(
-    #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
-) -> Result<AxumService, shuttle_runtime::Error> {
-    let redis_url = secrets.get("REDIS_URL").expect("REDIS_URL was not set");
-    let client = redis::Client::open(redis_url).map_err(anyhow::Error::new)?;
+async fn axum() -> Result<shuttle_axum::AxumService, shuttle_runtime::Error> {
+    let store = LinkStore::default();
 
-    // Set up rate limiting
-    let governor_conf = std::sync::Arc::new(
-        GovernorConfigBuilder::default()
-            .per_second(10) // replenish every interval
-            .burst_size(4)
-            .finish()
-            .unwrap(),
-    );
-
-    let governor_limiter = governor_conf.limiter().clone();
-    // a separate background task to clean up
-    std::thread::spawn(move || loop {
-        std::thread::sleep(std::time::Duration::from_secs(60));
-        governor_limiter.retain_recent();
-    });
+    cleanup_task(store.clone());
 
     let api_router = Router::new()
         .route("/", post(create_link))
         .route("/:id", get(get_data_view).delete(delete_link));
-    // Shuttle seems to run through a docker container so the peer address received here is from
-    // that meaning this effectely rate limits globally idk how to fix
-    // .layer(GovernorLayer {
-    //     config: governor_conf,
-    // });
 
     let router = Router::new()
         .route("/script.js", static_route!("script", "js"))
@@ -67,9 +43,9 @@ async fn axum(
         .route("/", static_route!("index", "html"))
         .merge(api_router)
         .layer(DefaultBodyLimit::max(5 * 1024)) // 5 kB max request body limit
-        .with_state(client);
+        .with_state(store);
 
-    Ok(AxumService(router))
+    Ok(router.into())
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -78,58 +54,80 @@ struct CreateLinkBody {
 }
 
 async fn create_link(
-    State(mut client): State<redis::Client>,
+    State(store): State<LinkStore>,
     Json(body): Json<CreateLinkBody>,
 ) -> AppResult<impl IntoResponse> {
-    // Create a random id in base64
-    let id = nanoid::nanoid!(LINK_LENGTH);
-    let _: () = client.set_ex(&id, body.data, 5 * 60)?; // Expire after 5 minutes
+    let mut store = store.write().await;
+
+    // Remove keys to save memory
+    if store.len() > MAX_LINKS {
+        let key = store.keys().next().unwrap().clone();
+        store.remove(&key);
+    }
+
+    // Make sure id does not already exist
+    let mut id = nanoid::nanoid!(LINK_LENGTH);
+    while store.contains_key(&id) {
+        id = nanoid::nanoid!(LINK_LENGTH);
+    }
+
+    store.insert(
+        id.clone(),
+        Link {
+            data: body.data,
+            expire_time: Instant::now() + EXPIRE_DURATION,
+        },
+    );
 
     Ok((StatusCode::OK, id))
 }
 
 async fn delete_link(
-    State(mut client): State<redis::Client>,
+    State(store): State<LinkStore>,
     Path(id): Path<String>,
 ) -> AppResult<StatusCode> {
-    let result: redis::RedisResult<()> = client.del(id);
+    let exists = store.write().await.remove(&id).is_some();
 
-    match result.is_ok() {
-        false => Ok(StatusCode::NOT_FOUND),
+    match exists {
         true => Ok(StatusCode::NO_CONTENT),
+        false => Ok(StatusCode::NOT_FOUND),
     }
 }
 
 // Get data from the link and deletes it
 async fn get_data_view(
-    State(mut client): State<redis::Client>,
+    State(store): State<LinkStore>,
     Path(id): Path<String>,
 ) -> AppResult<impl IntoResponse> {
-    // Check if request path is in the id format to prevent ranodm request from using the api
-    static CHARS: LazyLock<HashSet<char>> = LazyLock::new(|| nanoid::alphabet::SAFE.into());
-    if id.len() != LINK_LENGTH || !id.chars().all(|char| CHARS.contains(&char)) {
-        return Ok(not_found_page_response());
-    }
+    let store = store.read().await;
 
-    let (data, seconds_till_expire): (Option<String>, u64) =
-        redis::pipe().get(&id).ttl(&id).query(&mut client)?;
-
-    if let Some(data) = data {
+    if let Some(link) = store.get(&id) {
+        let expire_secs = (link.expire_time - Instant::now()).as_secs();
         let html = include_str!("view.html")
-            .replace("%DATA%", &data)
-            .replace("%TTL%", &seconds_till_expire.to_string());
-        Ok((StatusCode::OK, cached_header!("text/html"), Html(html)))
+            .replace("%DATA%", &link.data)
+            .replace("%EXPIRE_SECS%", &expire_secs.to_string());
+        Ok((StatusCode::OK, cached_header!("text/html", 0), Html(html)))
     } else {
-        Ok(not_found_page_response())
+        Ok((
+            StatusCode::NOT_FOUND,
+            cached_header!("text/html", 31536000),
+            Html(include_str!("static/not-found.html").to_owned()),
+        ))
     }
 }
 
-fn not_found_page_response<'a>() -> (StatusCode, [(HeaderName, &'a str); 2], Html<String>) {
-    (
-        StatusCode::NOT_FOUND,
-        cached_header!("text/html"),
-        Html(include_str!("static/not-found.html").to_owned()),
-    )
+fn cleanup_task(store: LinkStore) {
+    // A separate background task to clean up
+    tokio::spawn(async move {
+        loop {
+            let now = Instant::now();
+            tokio::time::sleep(CLEANUP_INTERVAL).await;
+            store
+                .write()
+                .await
+                .retain(|_, link| link.expire_time - now > Duration::ZERO);
+        }
+    });
 }
 
 // Make our own error that wraps `anyhow::Error`.
@@ -163,7 +161,7 @@ where
 macro_rules! static_route {
     ( $file:expr, $ext:expr ) => {{
         get((
-            cached_header!(concat!("text/", $ext)),
+            cached_header!(concat!("text/", $ext), 31536000),
             include_str!(concat!("static/", $file, ".", $ext)),
         ))
     }};
@@ -171,10 +169,13 @@ macro_rules! static_route {
 
 #[macro_export]
 macro_rules! cached_header {
-    ($content_type: expr) => {
+    ($content_type: expr, $time: literal) => {
         [
             (header::CONTENT_TYPE, $content_type),
-            (header::CACHE_CONTROL, "public,max-age=31536000,immutable"),
+            (
+                header::CACHE_CONTROL,
+                concat!("public,max-age=", $time, "immutable"),
+            ),
         ]
     };
 }
